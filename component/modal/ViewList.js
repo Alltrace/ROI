941652397b871312eebbf51f1cf8b32f232f9d37
Pratik Dhoke <pratikdhoke13@gmail.com>
2022-03-08 01:53:16 +0530

    feat: delete tag from list

--- component/modal/ViewList.js
+++ component/modal/ViewList.js
@@ -1,35 +1,47 @@
-import * as React from 'react';
-import { Modal, Portal, Text, Button,Icon,Chip, Dialog,Paragraph, Provider } from 'react-native-paper';
-import { TagPlusAPI } from '../../service/api';
+import * as React from "react";
+import {
+  Modal,
+  Portal,
+  Text,
+  Button,
+  Icon,
+  Chip,
+  Dialog,
+  Paragraph,
+  Provider,
+} from "react-native-paper";
+import { TagPlusAPI } from "../../service/api";
 
-const ViewList = ({data}) => {
+const ViewList = ({ data, handleDelete }) => {
   const [visible, setVisible] = React.useState(false);
   const showModal = () => setVisible(true);
   const hideModal = () => setVisible(false);
-  const containerStyle = {backgroundColor: 'white', };
+  const containerStyle = { backgroundColor: "white" };
 
-  const handleOrder = async() => {
+  const handleOrder = async () => {
     const res = await TagPlusAPI(data);
-    console.log(res, 'res order')
-  }
+    console.log(res, "res order");
+  };
 
   return (
     <Provider>
       <Portal>
-      <Dialog visible={visible} onDismiss={hideModal} >
-        <Dialog.Content>
-    {data.map((list, index) => (
-      <>
-        {console.log(list, 'map-list-check')}
-        <Chip onClose={()=> console.log('do nothing')} >{list.tag}</Chip>
-      </>
-
-    ))}
-    <Button onPress={handleOrder} mode='contained'>Proceed to order</Button>
-        </Dialog.Content>
-      </Dialog>  
+        <Dialog visible={visible} onDismiss={hideModal}>
+          <Dialog.Content>
+            {data.map((list, index) => (
+              <Chip key={index} onClose={() => handleDelete(index)}>
+                {list.tag}
+              </Chip>
+            ))}
+            {data.length > 0 && (
+              <Button onPress={handleOrder} mode="contained">
+                Proceed to order
+              </Button>
+            )}
+          </Dialog.Content>
+        </Dialog>
       </Portal>
-      <Button style={{marginTop: 30}} onPress={showModal}>
+      <Button style={{ marginTop: 30 }} onPress={showModal}>
         Show
       </Button>
     </Provider>
--- screen/TagScreen.js
+++ screen/TagScreen.js
@@ -1,22 +1,30 @@
-import React from 'react'
-import { Text } from 'react-native-paper'
-import { View,Button } from 'react-native'
-import Scanner from '../component/scanner/Scanner'
-import ViewList from '../component/modal/ViewList'
+import React from "react";
+import { Text } from "react-native-paper";
+import { View, Button } from "react-native";
+import Scanner from "../component/scanner/Scanner";
+import ViewList from "../component/modal/ViewList";
 
 const TagScreen = () => {
-  const [list, setList] = React.useState([{}])
-  const handleQRData = ({type: type, data: data}) => {
-    console.log(type,'--',data, 'QR data');
-    const tempList = {tag: data}
-    setList([...list, tempList]);
-  }
-  return (
-    <View style={{flex:1}}>
-         <Scanner handleChange={handleQRData} style={{padding:90}} />
-       <ViewList data={list}/>
-    </View>
-  )
-}
+  const [list, setList] = React.useState([]);
 
-export default TagScreen
+  const handleQRData = ({ type: type, data: data }) => {
+    // console.log(type, "--", data, "QR data");
+    const tempList = { tag: data };
+    setList([...list, tempList]);
+  };
+
+  const handleDeleteTag = (index) => {
+    const tempList = [...list];
+    tempList.splice(index, 1);
+    setList(tempList);
+    console.log(list, "list");
+  };
+  return (
+    <View style={{ flex: 1 }}>
+      <Scanner handleChange={handleQRData} style={{ padding: 90 }} />
+      <ViewList handleDelete={handleDeleteTag} data={list} />
+    </View>
+  );
+};
+
+export default TagScreen;
